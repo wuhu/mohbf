20beeaf82c65e5cc2b41924a410a5f9de747cf5e
Mirko Dietrich <mirko.dietrich@bccn-berlin.de>
2010-05-17 18:06:19 +0200

    small changes

--- 1.5.py
+++ 1.5.py
@@ -9,15 +9,19 @@ from m1_3 import get_patch, normalize_set
 image_list = listdir(IMAGE_PATH)
 patches = []
 xy = 41
-n = 100
-avpatch = np.zeros((xy, xy))
+n = 5000
+patches = []
 for i in range(n):
   im = import_pics(choice(image_list))
-  patch = get_patch(im,xy)
-  reference = patch[(xy-1)/2, (xy-1)/2]
-  avpatch = avpatch + patch * reference
+  patches.append(get_patch(im,xy))
+
+patches = normalize_set(patches)
+
+avpatch = np.zeros((xy, xy))
+for i in range(len(patches)):
+  reference = patches[i][(xy-1)/2, (xy-1)/2]
+  avpatch = avpatch + patches[i] * reference
   print i
-avpatch = avpatch / n
+mpl.figure()
 mpl.imshow(avpatch)
 mpl.show()
-  
